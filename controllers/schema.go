0276feb0e7dd2827c79bd9da9c4d1faec0e0c44e
DeniskaRediska <grj.den@gmail.com>
2019-07-26 11:33:18 +0300

    add(not found for schema request)

--- controllers/schema.go
+++ controllers/schema.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"isp-config-service/domain"
 	"isp-config-service/entity"
 	"isp-config-service/model"
@@ -11,5 +13,11 @@ var Schema = schema{}
 type schema struct{}
 
 func (schema) GetByModuleId(request domain.GetByModuleIdRequest) (*entity.ConfigSchema, error) {
-	return model.SchemaRep.GetSchemaByModuleId(request.ModuleId)
+	if response, err := model.SchemaRep.GetSchemaByModuleId(request.ModuleId); err != nil {
+		return nil, err
+	} else if response == nil {
+		return nil, status.Errorf(codes.NotFound, "schema with moduleId %d not found", request.ModuleId)
+	} else {
+		return response, nil
+	}
 }
