44961d8416c891a3f029105a0273e3f4ba112d41
Синельник Тимур Романович <sinelnik.timur@yandex.ru>
2026-08-24 09:43:44 +0300

    MSP-8394 Add required admin permission

--- routes/routes.go
+++ routes/routes.go
@@ -27,12 +27,23 @@ type Controllers struct {
 }
 
 func EndpointDescriptors() []cluster.EndpointDescriptor {
-	return endpointDescriptors(Controllers{})
+	c := Controllers{}
+	return concatEndpoints(
+		modulesDescriptors(c),
+		configsDescriptors(c),
+		variablesDescriptors(c),
+	)
 }
 
 func GrpcHandler(wrapper endpoint.Wrapper, c Controllers) *grpc.Mux {
 	muxer := grpc.NewMux()
-	for _, descriptor := range endpointDescriptors(c) {
+	for _, descriptor := range modulesDescriptors(c) {
+		muxer.Handle(descriptor.Path, wrapper.Endpoint(descriptor.Handler))
+	}
+	for _, descriptor := range configsDescriptors(c) {
+		muxer.Handle(descriptor.Path, wrapper.Endpoint(descriptor.Handler))
+	}
+	for _, descriptor := range variablesDescriptors(c) {
 		muxer.Handle(descriptor.Path, wrapper.Endpoint(descriptor.Handler))
 	}
 	return muxer
@@ -67,106 +78,143 @@ func HttpHandler(etpSrv *etp.Server, conf conf.Local, rqliteProxy, rqliteBackupP
 	return httpMux
 }
 
-// nolint:funlen
-func endpointDescriptors(c Controllers) []cluster.EndpointDescriptor {
+func modulesDescriptors(c Controllers) []cluster.EndpointDescriptor {
 	return []cluster.EndpointDescriptor{
-		// modules
 		{
 			Path:    "config/module/get_modules_info",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_view"),
 			Handler: c.ModuleApi.Status,
 		}, {
 			Path:    "config/module/delete_module",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_delete"),
 			Handler: c.ModuleApi.DeleteModule,
 		}, {
 			Path:    "config/routing/get_routes",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_view"),
 			Handler: c.ModuleApi.Connections,
 		}, {
 			Path:    "config/module/get_required_modules",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_view"),
 			Handler: c.ModuleApi.RequiredModules,
 		},
-		// configs
+	}
+}
+
+func configsDescriptors(c Controllers) []cluster.EndpointDescriptor {
+	return []cluster.EndpointDescriptor{
 		{
 			Path:    "config/config/get_active_config_by_module_name",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_view"),
 			Handler: c.ConfigApi.GetActiveConfigByModuleName,
 		}, {
 			Path:    "config/config/get_configs_by_module_id",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_view"),
 			Handler: c.ConfigApi.GetConfigsByModuleId,
 		}, {
 			Path:    "config/config/create_update_config",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_configuration_edit"),
 			Handler: c.ConfigApi.CreateUpdateConfig,
 		}, {
 			Path:    "config/config/get_config_by_id",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_view"),
 			Handler: c.ConfigApi.GetConfigById,
 		}, {
 			Path:    "config/config/mark_config_as_active",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_configuration_set_active"),
 			Handler: c.ConfigApi.MarkConfigAsActive,
 		}, {
 			Path:    "config/config/update_config_name",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_configuration_edit"),
 			Handler: c.ConfigApi.UpdateConfigName,
 		}, {
 			Path:    "config/config/delete_config",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_configuration_edit"),
 			Handler: c.ConfigApi.DeleteConfigs,
 		}, {
 			Path:    "config/config/sync",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_configuration_edit"),
 			Handler: c.ConfigApi.SyncConfig,
 		}, {
 			Path:    "config/config/get_all_version",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_view"),
 			Handler: c.ConfigHistoryApi.GetAllVersion,
 		}, {
 			Path:    "config/config/delete_version",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_history_delete_version"),
 			Handler: c.ConfigHistoryApi.DeleteConfigVersion,
 		}, {
 			Path:    "config/config/purge_versions",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_history_delete_version"),
 			Handler: c.ConfigHistoryApi.PurgeConfigVersions,
 		}, {
 			Path:    "config/schema/get_by_module_id",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_view"),
 			Handler: c.ConfigSchemaApi.SchemaByModuleId,
 		}, {
 			Path:    "config/schema/update_by_module_name",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("module_configuration_edit"),
 			Handler: c.ConfigSchemaApi.UpdateSchemaByModuleName,
 		},
-		// variables
+	}
+}
+
+func variablesDescriptors(c Controllers) []cluster.EndpointDescriptor {
+	return []cluster.EndpointDescriptor{
 		{
 			Path:    "config/variable/all",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("variable_view"),
 			Handler: c.VariableApi.All,
 		}, {
 			Path:    "config/variable/get_by_name",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("variable_view"),
 			Handler: c.VariableApi.GetByName,
 		}, {
 			Path:    "config/variable/create",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("variable_add"),
 			Handler: c.VariableApi.Create,
 		}, {
 			Path:    "config/variable/update",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("variable_edit"),
 			Handler: c.VariableApi.Update,
 		}, {
 			Path:    "config/variable/upsert",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("variable_edit"),
 			Handler: c.VariableApi.Upsert,
 		}, {
 			Path:    "config/variable/delete",
 			Inner:   true,
+			Extra:   cluster.RequireAdminPermission("variable_delete"),
 			Handler: c.VariableApi.Delete,
 		},
 	}
+}
+
+func concatEndpoints(endpoints ...[]cluster.EndpointDescriptor) []cluster.EndpointDescriptor {
+	var result []cluster.EndpointDescriptor
+	for _, c := range endpoints {
+		result = append(result, c...)
+	}
+	return result
 }
